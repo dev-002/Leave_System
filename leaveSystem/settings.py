fe0a4fc894c95e9bad8f0ce4da8d945dafc30df0
ishanjoshi <ishanjoshiian@gmail.com>
2024-04-07 07:42:41 +0530

    staff app

--- leaveSystem/settings.py
+++ leaveSystem/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'leaveSystem',
     'accounts',
+    'staff',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
